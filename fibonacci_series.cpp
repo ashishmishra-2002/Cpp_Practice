3479ca0a39acbcf1ff55ed23dfb49b4fe591dbec
Ashish Mishra <akmishra0982@gmail.com>
2023-08-27 16:49:48 +0530

    Update file

--- fibonacci_series.cpp
+++ fibonacci_series.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main(){
 
     int num1=0,num2=1,num3;
-    cout << num1 << " " << num2 << " " <<endl;
+    cout << num1 << " " << num2 << " " ;
     for(int i=0;i<10;i++){
         num3=num1+num2;
-        cout << num3 << endl;
+        cout << num3 << " ";
         num1=num2;
         num2=num3;
     }
